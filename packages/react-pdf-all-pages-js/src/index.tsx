e0ede893868d3ee3445fc5cb33461f611230e965
Patrick Van Weelden <pvanweel@yahoo.com>
2024-09-03 16:41:35 -0500

    updated tests

--- packages/react-pdf-all-pages-js/src/index.tsx
+++ packages/react-pdf-all-pages-js/src/index.tsx
@@ -183,7 +183,9 @@ export const usePdf = ({
         return;
       }
     }
-    drawPDFPages();
+    if (pdfDocument) {
+      drawPDFPages();
+    }
   }, [
     pdfLocation,
     pdfDocument,
---
+++ packages/react-pdf-all-pages-js/test/index.test.tsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import { render, wait, waitForDomChange } from '@testing-library/react';
+import Pdf from '../src';
+import { DocumentInitParameters } from 'pdfjs-dist/types/src/display/api';
+
+jest.mock('pdfjs-dist', () => ({
+  version: '1.0',
+  GlobalWorkerOptions: {
+    workerSrc: '',
+  },
+  getDocument: jest.fn((config: DocumentInitParameters) => ({
+    promise: config.url?.includes('fail_document')
+      ? Promise.reject()
+      : Promise.resolve({
+          drawPDFPages: jest.fn(() =>
+            // getPage: jest.fn(() =>
+            config.url?.includes('fail_page')
+              ? Promise.reject()
+              : Promise.resolve({
+                  getViewport: jest.fn(() => ({ width: 0, height: 0 })),
+                  render: jest.fn(() => ({
+                    promise: config.url?.includes('fail_render')
+                      ? Promise.reject()
+                      : Promise.resolve(),
+                  })),
+                })
+          ),
+        }),
+  })),
+}));
+
+describe('Pdf', () => {
+  it('renders children', async () => {
+    const { getByText } = render(
+      <Pdf file="basic.pdf">
+        {({ canvas }) => (
+          <div>
+            {canvas}
+            <div>Test</div>
+          </div>
+        )}
+      </Pdf>
+    );
+
+    await waitForDomChange();
+
+    getByText('Test');
+  });
+
+  it('calls render function with proper params', async () => {
+    const renderFunc = jest.fn(({ canvas }) => canvas);
+
+    render(<Pdf file="basic.pdf">{renderFunc}</Pdf>);
+
+    expect(renderFunc).toBeCalledWith({
+      canvas: expect.any(Object),
+      pdfDocument: undefined,
+      pdfPage: undefined,
+    });
+
+    await wait();
+
+    expect(renderFunc).toBeCalledWith({
+      canvas: expect.any(Object),
+      pdfDocument: expect.any(Object),
+      pdfPage: expect.any(Object),
+    });
+  });
+
+  describe('callbacks', () => {
+    const onDocLoadSuccess = jest.fn();
+    const onDocLoadFail = jest.fn();
+    const onInvalidLocation = jest.fn();
+
+    beforeEach(() => {
+      onDocLoadSuccess.mockClear();
+      onDocLoadFail.mockClear();
+      onInvalidLocation.mockClear();
+    });
+
+    const renderPdf = (file: string) =>
+      render(
+        <Pdf
+          file={file}
+          onDocumentLoadSuccess={onDocLoadSuccess}
+          onDocumentLoadFail={onDocLoadFail}
+          onInvalidLocation={onInvalidLocation}
+        >
+          {({ canvas }) => canvas}
+        </Pdf>
+      );
+
+    it('calls proper callbacks when fully successful', async () => {
+      renderPdf('basic.33e35a62.pdf');
+
+      await wait();
+
+      expect(onDocLoadSuccess).toBeCalledWith(expect.any(Object));
+      expect(onDocLoadFail).not.toBeCalled();
+      expect(onInvalidLocation).not.toBeCalled();
+    });
+
+    it('calls proper callbacks when render failed', async () => {
+      renderPdf('fail_render');
+
+      await wait();
+
+      expect(onDocLoadSuccess).toBeCalledWith(expect.any(Object));
+      expect(onDocLoadFail).not.toBeCalled();
+      expect(onInvalidLocation).not.toBeCalled();
+    });
+
+    it('calls proper callbacks when page load failed', async () => {
+      renderPdf('fail_page');
+
+      await wait();
+
+      expect(onDocLoadSuccess).toBeCalledWith(expect.any(Object));
+      expect(onDocLoadFail).not.toBeCalled();
+      expect(onInvalidLocation).not.toBeCalled();
+    });
+
+    it('calls proper callbacks when document load failed', async () => {
+      renderPdf('fail_document');
+
+      await wait();
+
+      expect(onDocLoadSuccess).not.toBeCalled();
+      expect(onDocLoadFail).toBeCalled();
+      expect(onInvalidLocation).not.toBeCalled();
+    });
+  });
+});
